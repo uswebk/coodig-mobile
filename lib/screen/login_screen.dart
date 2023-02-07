09cc7412d35ab6210512a9b77fcb05dd438f0c78
uswebk <usweb111@gmail.com>
2023-02-07 21:43:44 +0900

    feature: add base of login screen

---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [Text('LoginPage')],
+        ),
+      ),
+    );
+  }
+}
