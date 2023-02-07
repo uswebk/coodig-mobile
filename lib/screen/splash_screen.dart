b64096442f06090b60338c89e78c3636e931c3d1
uswebk <usweb111@gmail.com>
2023-02-08 00:33:32 +0900

    feature: Splash to LoginPage

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,10 +1,18 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'login_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 2), () {
+      Get.off(const LoginScreen());
+    });
     return Scaffold(
       backgroundColor: Colors.orangeAccent,
       body: Center(
