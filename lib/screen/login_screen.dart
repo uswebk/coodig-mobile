10d5346ab6fc4e88a349bd30b682993b19cfa0bc
uswebk <usweb111@gmail.com>
2023-02-10 23:11:12 +0900

    update: make ui in login screen

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -5,11 +5,52 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: const [Text('LoginPage')],
+          children: [
+            Container(
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: emailController,
+                    decoration: const InputDecoration(labelText: 'Email'),
+                    onChanged: (String value) {},
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(labelText: 'Password'),
+                    onChanged: (String value) {},
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          if (emailController.text.isEmpty) return;
+                          if (passwordController.text.isEmpty) return;
+
+                          // TODO: Login Action
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orangeAccent,
+                        ),
+                        child: const Text('Login')),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
