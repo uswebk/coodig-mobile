9129a6f9de526594f8b2edf7080c8d72ab2c604e
uswebk <usweb111@gmail.com>
2023-02-17 00:02:05 +0900

    feature: add LocalStorage

---
+++ lib/core/local_storage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class LocalStorage {
+  final _storage = const FlutterSecureStorage();
+
+  Future<void> addToken(Map<String, dynamic> token) async {
+    await _storage.write(key: 'accessToken', value: token['access'].toString());
+    await _storage.write(
+        key: 'refreshToken', value: token['refresh'].toString());
+  }
+
+  Future<Map<String, String>> readAll() async {
+    return await _storage.readAll();
+  }
+}
