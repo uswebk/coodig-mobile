116912248fd7c6572e8fcff443b2d7664b1dfe63
uswebk <usweb111@gmail.com>
2023-02-16 22:36:24 +0900

    feature: add auth service

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -1,10 +1,22 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../repository/auth_repository.dart';
+
+final repository = Provider((ref) => ref.watch(authRepositoryProvider));
+
+final authServiceProvider =
+    Provider((ref) => AuthService(ref.watch(repository)));
+
 class AuthService {
+  AuthService(this._authRepository);
+
+  final AuthRepository _authRepository;
+
   Future<String> login(String email, String password) async {
-    // TODO API Access
-    return 'Token';
+    final response = await _authRepository.login(email, password);
+
+    print(response);
+    // TODO: Convert
+    return 'hoge';
   }
 }
-
-final authSserviceProvider = Provider<AuthService>((ref) => AuthService());
