954caaff75a9d227c9c8a5aa6069eaf853af1cf0
uswebk <usweb111@gmail.com>
2023-02-19 19:15:10 +0900

    update: add Token to LocalStorage

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../core/local_storage.dart';
 import '../repository/auth_repository.dart';
 
 final repository = Provider((ref) => ref.watch(authRepositoryProvider));
@@ -11,12 +14,19 @@ class AuthService {
   AuthService(this._authRepository);
 
   final AuthRepository _authRepository;
+  final _localStorage = LocalStorage();
 
-  Future<String> login(String email, String password) async {
+  Future<bool> login(String email, String password) async {
     final response = await _authRepository.login(email, password);
 
-    print(response);
-    // TODO: Convert
-    return 'hoge';
+    if (response.statusCode == 200) {
+      var body = json.decode(response.body);
+      Map<String, dynamic> token = Map<String, dynamic>.from(body['token']);
+      await _localStorage.addToken(token);
+
+      return true;
+    }
+
+    return false;
   }
 }
