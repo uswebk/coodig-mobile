3c174a1f4e17e232ceaee209e2e826b1eeb4e154
uswebk <usweb111@gmail.com>
2023-02-20 00:04:02 +0900

    refactor: add typehint

--- lib/core/http_client.dart
+++ lib/core/http_client.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 const host = 'http://127.0.0.1:9999';
 
 class HttpClient {
-  Future<http.Response> post(String path, body) async {
+  Future<http.Response> post(String path, Map<String, String> body) async {
     return await http.post(Uri.parse(host + path),
         headers: {'content-type': 'application/json'}, body: jsonEncode(body));
   }
