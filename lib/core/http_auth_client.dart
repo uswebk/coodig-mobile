ceb6da5f7403ded4e1c35f1ce9796efd45675b21
uswebk <usweb111@gmail.com>
2023-02-22 00:03:13 +0900

    update: add queryParameters to auth client

--- lib/core/http_auth_client.dart
+++ lib/core/http_auth_client.dart
@@ -5,8 +5,9 @@ const host = 'http://127.0.0.1:9999';
 class HttpAuthClient {
   Future<http.Response> get(
       String path, Map<String, String> query, String accessToken) async {
+    final uri = Uri.parse(host + path).replace(queryParameters: query);
     return await http.get(
-      Uri.parse(host + path),
+      uri,
       headers: {
         'content-type': 'application/json',
         'Authorization': 'Bearer $accessToken'
