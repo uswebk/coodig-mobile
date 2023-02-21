1c462a23b2772675f7e7f25329f0628713814410
uswebk <usweb111@gmail.com>
2023-02-21 20:41:12 +0900

    feature: add user repository

---
+++ lib/repository/user_repository.dart
@@ -0,0 +1,24 @@
+import 'package:coodig_mobile/core/local_storage.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+
+import '../core/http_auth_client.dart';
+
+final userRepositoryProvider =
+    Provider((ref) => UserRepository(HttpAuthClient(), LocalStorage()));
+
+class UserRepository {
+  UserRepository(this._httpClient, this._localStorage);
+
+  final HttpAuthClient _httpClient;
+  final LocalStorage _localStorage;
+
+  Future<http.Response?> fetchUser() async {
+    String? accessToken = await _localStorage.getAccessToken();
+
+    if (accessToken != null) {
+      return await _httpClient.get('/api/v1/accounts/me/', {}, accessToken);
+    }
+    return null;
+  }
+}
