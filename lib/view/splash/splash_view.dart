2075a78be04fe5f8bc03f91cc20cc1ecaf28c38b
uswebk <usweb111@gmail.com>
2023-02-26 00:22:43 +0900

    update: If get a user, go to the dashboard

--- lib/view/splash/splash_view.dart
+++ lib/view/splash/splash_view.dart
@@ -1,16 +1,26 @@
 import 'dart:async';
 
-import 'package:coodig_mobile/view/login/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
 
-class SplashScreen extends StatelessWidget {
+import '../../provider/user_provider.dart';
+import '../dashboard/dashboard_screen.dart';
+import '../login/login_screen.dart';
+
+class SplashScreen extends ConsumerWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    Timer(const Duration(seconds: 2), () {
-      Get.off(const LoginScreen());
+  Widget build(BuildContext context, WidgetRef ref) {
+    Timer(const Duration(seconds: 2), () async {
+      await ref.watch(userNotifierProvider.notifier).fetchUser();
+      bool isAuth = ref.read(userNotifierProvider).isAuth;
+      if (isAuth) {
+        Get.off(const DashboardScreen());
+      } else {
+        Get.off(const LoginScreen());
+      }
     });
     return Scaffold(
       backgroundColor: Colors.orangeAccent,
