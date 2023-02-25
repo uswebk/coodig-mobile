c4056892776ad483e9bf2edaa71d6cd83d1f49d9
uswebk <usweb111@gmail.com>
2023-02-25 14:41:36 +0900

    feature: add Dashboard Screen

---
+++ lib/view/dashboard/dashboard_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [Text('Dashboard')],
+        ),
+      ),
+    );
+  }
+}
