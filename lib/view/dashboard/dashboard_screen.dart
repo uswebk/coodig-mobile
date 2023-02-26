312aa212463c456699ed88ff3cde670246e3f901
uswebk <usweb111@gmail.com>
2023-02-27 00:13:12 +0900

    update: apply riverpod in Dashboard

--- lib/view/dashboard/dashboard_screen.dart
+++ lib/view/dashboard/dashboard_screen.dart
@@ -1,15 +1,21 @@
+import 'package:coodig_mobile/provider/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends ConsumerWidget {
   const DashboardScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.read(userNotifierProvider);
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: const [Text('Dashboard')],
+          children: [
+            const Text('Dashboard'),
+            Text(state.name),
+          ],
         ),
       ),
     );
