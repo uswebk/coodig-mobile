ba6ac00d9da3d3da26bf6b398cd7ad5a11184109
uswebk <usweb111@gmail.com>
2023-03-01 00:14:30 +0900

    update: fetch user in AuthProvider

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,17 +1,33 @@
 import 'package:coodig_mobile/service/auth_service.dart';
+import 'package:coodig_mobile/service/user_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final service = Provider((ref) => ref.watch(authServiceProvider));
+import '../model/user.dart';
 
-final authNotifierProvider =
-    StateNotifierProvider((ref) => AuthNotifier(ref.watch(service)));
+final authService = Provider((ref) => ref.watch(authServiceProvider));
+final userService = Provider((ref) => ref.watch(userServiceProvider));
 
-class AuthNotifier extends StateNotifier {
-  AuthNotifier(this._authService) : super(0);
+final authNotifierProvider = StateNotifierProvider<AuthNotifier, User?>((ref) {
+  return AuthNotifier(ref.watch(authService), ref.watch(userService));
+});
+
+class AuthNotifier extends StateNotifier<User?> {
+  AuthNotifier(this._authService, this._userService) : super(null);
 
   final AuthService _authService;
+  final UserService _userService;
+
+  Future<void> fetchUser() async {
+    state = await _userService.fetchUser();
+  }
 
   Future<bool> login(String email, String password) async {
     return _authService.login(email, password);
   }
 }
+
+final isAuthenticatedProvider = Provider<bool>((ref) {
+  final User? user = ref.watch(authNotifierProvider);
+
+  return user != null;
+});
