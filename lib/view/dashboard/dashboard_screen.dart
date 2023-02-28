6ba1b83a60aed2120f2b0aaaef7ea42b0069544c
uswebk <usweb111@gmail.com>
2023-02-28 21:03:31 +0900

    update: add sign out button

--- lib/view/dashboard/dashboard_screen.dart
+++ lib/view/dashboard/dashboard_screen.dart
@@ -9,13 +9,30 @@ class DashboardScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.read(userNotifierProvider);
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text('Dashboard'),
-            Text(state.name),
-          ],
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('Dashboard'),
+              Text('${state.name} (${state.email})'),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () async {
+                      // Todo: Reset Token & push LoginScreen
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                    ),
+                    child: const Text('Logout')),
+              ),
+            ],
+          ),
         ),
       ),
     );
