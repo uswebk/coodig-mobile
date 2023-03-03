d2641eeaa6e674cf97edce54ca166f34fac55362
uswebk <usweb111@gmail.com>
2023-03-03 21:10:35 +0900

    update: add emailVerifiedAt to UserModel

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,16 +1,22 @@
 class User {
   final String name;
   final String email;
+  final DateTime? emailVerifiedAt;
 
   User({
     required this.name,
     required this.email,
+    required this.emailVerifiedAt,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
+    DateTime? emailVerifiedAt = json['email_verified_at'] != null
+        ? DateTime.parse(json['email_verified_at'])
+        : null;
+
     return User(
-      name: json['name'],
-      email: json['email'],
-    );
+        name: json['name'],
+        email: json['email'],
+        emailVerifiedAt: emailVerifiedAt);
   }
 }
