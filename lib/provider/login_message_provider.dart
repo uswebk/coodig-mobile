9b636dba24cb40e11960b6f60ae67ae295c8d43f
uswebk <usweb111@gmail.com>
2023-03-04 01:36:27 +0900

    feature: add loginMessageProvider

---
+++ lib/provider/login_message_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final loginMessageProvider =
+    StateNotifierProvider<LoginMessageNotifier, String>((ref) {
+  return LoginMessageNotifier();
+});
+
+class LoginMessageNotifier extends StateNotifier<String> {
+  LoginMessageNotifier() : super('');
+
+  void setMessage(Map<String, dynamic> errors) {
+    String errorMessage = '';
+
+    errors.forEach((key, value) {
+      errorMessage += '${value[0]}\n';
+    });
+
+    RegExp exp = RegExp(r'\n+$');
+
+    state = errorMessage.replaceAll(exp, '');
+  }
+}
