b92f7ca3a104ce56c34df1828caa3e87df7179df
uswebk <usweb111@gmail.com>
2023-03-10 00:24:04 +0900

    feature: add Otp Provider

---
+++ lib/provider/otp_provider.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final otpStateProvider =
+    StateNotifierProvider<OtpStateNotifier, OtpState>((ref) {
+  final controllers = List.generate(
+      6, (index) => TextEditingController(text: ''),
+      growable: false);
+  return OtpStateNotifier(OtpState(controllers));
+});
+
+class OtpState {
+  final List<TextEditingController> controllers;
+  final String otp;
+  final bool isButtonEnabled;
+  final bool isLoading;
+
+  OtpState(this.controllers,
+      {this.otp = '', this.isButtonEnabled = false, this.isLoading = false});
+}
+
+class OtpStateNotifier extends StateNotifier<OtpState> {
+  OtpStateNotifier(OtpState state) : super(state);
+
+  void updateOtp() {
+    String otp = state.controllers.map((controller) => controller.text).join();
+    bool isButtonEnabled =
+        state.controllers.every((controller) => controller.text.isNotEmpty);
+    state =
+        OtpState(state.controllers, otp: otp, isButtonEnabled: isButtonEnabled);
+  }
+
+  void sendOtp() {
+    // Execute Otp API
+  }
+}
