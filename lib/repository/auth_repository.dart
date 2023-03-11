ea1205410f02c9c0ae5c921f8f4ffaaaea8a0e52
uswebk <usweb111@gmail.com>
2023-03-11 11:14:04 +0900

    refactor: Integrate HttpClient

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -2,23 +2,37 @@ import 'package:coodig_mobile/core/http_client.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 
+import '../core/local_storage.dart';
+
 final authRepositoryProvider = Provider((ref) => AuthRepository(HttpClient()));
 
 class AuthRepository {
   AuthRepository(this._httpClient);
 
   final HttpClient _httpClient;
+  final _localStorage = LocalStorage();
 
   Future<http.Response> login(String email, String password) async {
-    return await _httpClient.post('/api/v1/accounts/login/', {
-      'email': email,
-      'password': password,
-    });
+    return await _httpClient.post(
+        '/api/v1/accounts/login/',
+        {
+          'email': email,
+          'password': password,
+        },
+        '');
   }
 
   Future<http.Response> refresh(String refreshToken) async {
-    return await _httpClient.post('/api/token/refresh/', {
-      'refresh': refreshToken,
-    });
+    return await _httpClient.post(
+        '/api/token/refresh/',
+        {
+          'refresh': refreshToken,
+        },
+        '');
+  }
+
+  Future<http.Response> sendOtp(String otp, String email) async {
+    String accessToken = await _localStorage.getAccessToken() ?? '';
+    return await _httpClient.post('/api/otp/verify/', {}, accessToken);
   }
 }
--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -2,15 +2,14 @@ import 'package:coodig_mobile/core/local_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 
-import '../core/http_auth_client.dart';
+import '../core/http_client.dart';
 
-final userRepositoryProvider =
-    Provider((ref) => UserRepository(HttpAuthClient()));
+final userRepositoryProvider = Provider((ref) => UserRepository(HttpClient()));
 
 class UserRepository {
   UserRepository(this._httpClient);
 
-  final HttpAuthClient _httpClient;
+  final HttpClient _httpClient;
   final _localStorage = LocalStorage();
 
   Future<http.Response> fetchMe() async {
