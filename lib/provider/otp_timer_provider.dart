27c66179ba8ea5101da6ed79130eb5b8b85c2600
uswebk <usweb111@gmail.com>
2023-03-25 01:24:15 +0900

    feature: add Otp Timer Provider

---
+++ lib/provider/otp_timer_provider.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+
+import 'package:coodig_mobile/model/otp.dart';
+import 'package:coodig_mobile/service/auth_service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../service/otp_service.dart';
+
+final authService = Provider((ref) => ref.watch(authServiceProvider));
+final otpService = Provider((ref) => ref.watch(otpServiceProvider));
+
+final otpTimerStateProvider =
+    StateNotifierProvider<OtpTimerNotifier, OtpState?>((ref) {
+  return OtpTimerNotifier(ref.watch(otpService));
+});
+
+class OtpState {
+  int minutes;
+  int seconds;
+  bool isCalled;
+  OtpState({this.minutes = 0, this.seconds = 0, this.isCalled = false});
+}
+
+class OtpTimerNotifier extends StateNotifier<OtpState> {
+  OtpTimerNotifier(this._otpService) : super(OtpState());
+
+  final OtpService _otpService;
+
+  Future<void> startTimer() async {
+    if (!state.isCalled) {
+      Otp? otp = await _otpService.fetch();
+
+      int minutes = 0;
+      int seconds = 0;
+
+      if (otp != null) {
+        DateTime now = DateTime.parse(DateTime.now()
+            .toString()
+            .substring(0, DateTime.now().toString().length - 7));
+
+        DateTime expirationAt = DateTime.parse(otp.expirationAt
+            .toString()
+            .substring(0, otp.expirationAt.toString().length - 5));
+
+        Duration difference = expirationAt.difference(now);
+
+        if (!difference.isNegative) {
+          minutes = difference.inMinutes;
+          seconds = difference.inSeconds.remainder(60);
+        }
+      }
+
+      state = OtpState(minutes: minutes, seconds: seconds, isCalled: true);
+
+      Timer.periodic(const Duration(seconds: 1), (timer) {
+        if (state.minutes == 0 && state.seconds == 0) {
+          timer.cancel();
+        } else if (state.seconds == 0) {
+          int minutes = state.minutes - 1;
+          int seconds = 59;
+          state = OtpState(minutes: minutes, seconds: seconds, isCalled: true);
+        } else {
+          int seconds = state.seconds - 1;
+          state = OtpState(
+              minutes: state.minutes, seconds: seconds, isCalled: true);
+        }
+      });
+    }
+  }
+}
