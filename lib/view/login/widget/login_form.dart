5314a596e1460dde19ca2a987fadaacfbc23b225
uswebk <usweb111@gmail.com>
2023-04-03 20:24:16 +0900

    refactor: login form widget

---
+++ lib/view/login/widget/login_form.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../provider/auth_provider.dart';
+import '../../../provider/login_provider.dart';
+
+class LoginForm extends ConsumerWidget {
+  const LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final formKey = GlobalKey<FormState>();
+
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+
+    LoginState state = ref.watch(loginStateProvider);
+
+    return Form(
+      key: formKey,
+      child: Center(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: (state.errorMessage != '')
+                  ? Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.red[50],
+                        borderRadius: BorderRadius.circular(4.0),
+                        border: Border.all(color: Colors.red.shade100),
+                      ),
+                      child: Text(
+                        state.errorMessage,
+                        style: TextStyle(color: Colors.red.shade300),
+                      ),
+                    )
+                  : Container(),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      prefixIcon: Icon(Icons.email),
+                      filled: true,
+                      fillColor: Colors.white60,
+                      border: InputBorder.none,
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
+                    },
+                    onChanged: (String value) {},
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      filled: true,
+                      fillColor: Colors.white60,
+                      border: InputBorder.none,
+                      prefixIcon: Icon(Icons.lock),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your password';
+                      }
+
+                      if (value.length <= 6) {
+                        return 'Password must be at least 6 characters';
+                      }
+                      return null;
+                    },
+                    onChanged: (String value) {},
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 40,
+                    child: ElevatedButton(
+                        onPressed: () async {
+                          if (formKey.currentState!.validate()) {
+                            formKey.currentState!.save();
+                            String email = emailController.text;
+                            String password = passwordController.text;
+                            ref
+                                .read(loginStateProvider.notifier)
+                                .setLoading(true);
+                            await ref
+                                .watch(authStateProvider.notifier)
+                                .login(email, password);
+                            ref
+                                .read(loginStateProvider.notifier)
+                                .setLoading(false);
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orangeAccent,
+                        ),
+                        child: const Text('Login')),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
