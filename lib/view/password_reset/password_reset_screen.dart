93aa9ed1662226b86afb24172391f9dc73473b87
uswebk <usweb111@gmail.com>
2023-04-09 02:13:35 +0900

    feature: add password reset screen

---
+++ lib/view/password_reset/password_reset_screen.dart
@@ -0,0 +1,34 @@
+import 'package:coodig_mobile/view/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get.dart';
+
+// ignore: must_be_immutable
+class PasswordResetScreen extends ConsumerWidget {
+  PasswordResetScreen(this.link, {super.key});
+  String link;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Get.to(const SplashScreen());
+          },
+        ),
+        backgroundColor: Colors.orangeAccent,
+        title: const Text('Password Reset'),
+        elevation: 0,
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text(link)),
+        ],
+      ),
+    );
+  }
+}
