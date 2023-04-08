2ccfd4770203082e1187e0a7944fe5acb66fb13f
uswebk <usweb111@gmail.com>
2023-04-09 01:55:35 +0900

    update: use deeplink

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:coodig_mobile/view/dashboard/dashboard_screen.dart';
-import 'package:coodig_mobile/view/login/login_screen.dart';
+import 'package:coodig_mobile/service/deeplink_service.dart';
 import 'package:coodig_mobile/view/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
-import 'package:uni_links/uni_links.dart';
 
 void main() async {
   runApp(const ProviderScope(child: MyApp()));
@@ -29,33 +27,15 @@ class MyApp extends ConsumerWidget {
         return null;
       },
       onGenerateInitialRoutes: (initialRoute) {
-        Widget screen;
-
-        if (initialRoute != '/') {
-          screen = LoginScreen();
-        } else {
-          screen = const SplashScreen();
-        }
-
         return [
           MaterialPageRoute(
             builder: (context) {
-              initUniLinks(context);
-              return screen;
+              DeeplinkService().initDeeplink();
+              return const SplashScreen();
             },
           ),
         ];
       },
     );
   }
-}
-
-void initUniLinks(BuildContext context) {
-  linkStream.listen((String? link) {
-    if (link != null && link.isNotEmpty) {
-      Get.to(const DashboardScreen());
-    }
-  }, onError: (err) {
-    print("Error listening to links: $err");
-  });
 }
