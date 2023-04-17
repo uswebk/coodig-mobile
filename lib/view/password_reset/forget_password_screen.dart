911296a71d65d702b9e9ae21ec1e9e9f017d06d1
uswebk <usweb111@gmail.com>
2023-04-18 00:34:02 +0900

    feature: forget password screen

--- lib/view/password_reset/forget_password_screen.dart
+++ lib/view/password_reset/forget_password_screen.dart
@@ -8,7 +8,10 @@ class ForgetPasswordScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final formKey = GlobalKey<FormState>();
+    final TextEditingController emailController = TextEditingController();
     return Scaffold(
+        backgroundColor: Colors.grey[300],
         appBar: AppBar(
           leading: IconButton(
             icon: const Icon(Icons.arrow_back_ios),
@@ -20,8 +23,68 @@ class ForgetPasswordScreen extends ConsumerWidget {
           title: const Text('Forget Password'),
           elevation: 0,
         ),
-        body: const Center(
-          child: Text('Forget Password'),
+        body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: SingleChildScrollView(
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  const Text(
+                    'Enter your email address and we\'ll send you a link to reset your password.',
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      prefixIcon: Icon(Icons.email),
+                      filled: true,
+                      fillColor: Colors.white60,
+                      border: InputBorder.none,
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
+                    },
+                    onChanged: (String value) {},
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 45,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        if (formKey.currentState!.validate()) {
+                          formKey.currentState!.save();
+
+                          String email = emailController.text;
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orangeAccent,
+                      ),
+                      child: const Text('Send Email'),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                ],
+              ),
+            ),
+          ),
         ));
   }
 }
