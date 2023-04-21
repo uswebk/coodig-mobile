2d80e139704ade1019362b10789cc1c86bdb29bf
uswebk <usweb111@gmail.com>
2023-04-21 21:18:16 +0900

    feature: add signed uri verify

--- lib/service/deeplink_service.dart
+++ lib/service/deeplink_service.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:coodig_mobile/view/splash/splash_screen.dart';
+import 'package:crypto/crypto.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:uni_links/uni_links.dart';
 
@@ -26,5 +30,29 @@ class DeeplinkService {
       default:
         Get.off(const SplashScreen());
     }
+  }
+
+  bool verifySignedUri(String uri) {
+    final segment = uri.split(':');
+
+    final expireTime = double.parse(segment[2]);
+    final now = DateTime.now().millisecondsSinceEpoch / 1000;
+
+    if (now > expireTime) {
+      return false;
+    }
+
+    String link = '${segment[0]}:${segment[1]}';
+    String signedUrl = '$link:$expireTime';
+
+    var hmacSha256 =
+        Hmac(sha256, utf8.encode(dotenv.env['URI_SECRET_KEY'].toString()));
+    final digest = hmacSha256.convert(utf8.encode(signedUrl));
+
+    if (digest.toString() != segment[3].toString()) {
+      return false;
+    }
+
+    return true;
   }
 }
