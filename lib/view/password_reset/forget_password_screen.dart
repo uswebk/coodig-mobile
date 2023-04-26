8700c357b1e05b8a882d9ce0b7e894a4258d04dd
uswebk <usweb111@gmail.com>
2023-04-26 20:37:09 +0900

    refactor: layout

--- lib/view/password_reset/forget_password_screen.dart
+++ lib/view/password_reset/forget_password_screen.dart
@@ -1,94 +1,139 @@
+import 'package:coodig_mobile/provider/reset_password_provider.dart';
 import 'package:coodig_mobile/view/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class ForgetPasswordScreen extends ConsumerWidget {
   const ForgetPasswordScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var state = ref.watch(resetPasswordStateProvider);
     final formKey = GlobalKey<FormState>();
     final TextEditingController emailController = TextEditingController();
+
     return Scaffold(
-        backgroundColor: Colors.grey[300],
-        appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios),
-            onPressed: () {
-              Get.to(const SplashScreen());
-            },
-          ),
-          backgroundColor: Colors.orangeAccent,
-          title: const Text('Forget Password'),
-          elevation: 0,
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Get.to(const SplashScreen());
+          },
         ),
-        body: Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const Text(
-                  'Enter your email address and we\'ll send you a link to reset your password.',
-                  style: TextStyle(
-                    color: Colors.black54,
-                    fontSize: 18,
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Form(
-                  key: formKey,
+        backgroundColor: Colors.orangeAccent,
+        title: const Text('Forget Password'),
+        elevation: 0,
+      ),
+      body: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: SingleChildScrollView(
                   child: Column(
                     children: [
-                      TextFormField(
-                        controller: emailController,
-                        decoration: const InputDecoration(
-                          labelText: 'Email',
-                          prefixIcon: Icon(Icons.email),
-                          filled: true,
-                          fillColor: Colors.white60,
-                          border: InputBorder.none,
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Please enter your email';
-                          }
-                          return null;
-                        },
-                        onChanged: (String value) {},
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(
+                      Container(
                         width: double.infinity,
-                        height: 45,
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            if (formKey.currentState!.validate()) {
-                              formKey.currentState!.save();
-
-                              String email = emailController.text;
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.orangeAccent,
-                          ),
-                          child: const Text('Send Email'),
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: (state.errorMessage != '')
+                            ? Container(
+                                padding: const EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  color: Colors.red[50],
+                                  borderRadius: BorderRadius.circular(4.0),
+                                  border:
+                                      Border.all(color: Colors.red.shade100),
+                                ),
+                                child: Text(
+                                  state.errorMessage,
+                                  style: TextStyle(color: Colors.red.shade300),
+                                ),
+                              )
+                            : Container(),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      const Text(
+                        'Enter your email address and we\'ll send you a link to reset your password.',
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 18,
                         ),
                       ),
                       const SizedBox(
-                        height: 150,
+                        height: 30,
+                      ),
+                      Form(
+                        key: formKey,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              controller: emailController,
+                              decoration: const InputDecoration(
+                                labelText: 'Email',
+                                prefixIcon: Icon(Icons.email),
+                                filled: true,
+                                fillColor: Colors.white60,
+                                border: InputBorder.none,
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Please enter your email';
+                                }
+                                return null;
+                              },
+                              onChanged: (String value) {},
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            SizedBox(
+                              width: double.infinity,
+                              height: 45,
+                              child: ElevatedButton(
+                                onPressed: () async {
+                                  if (formKey.currentState!.validate()) {
+                                    formKey.currentState!.save();
+
+                                    String email = emailController.text;
+
+                                    await ref
+                                        .read(
+                                            resetPasswordStateProvider.notifier)
+                                        .sendResetPassword(email);
+                                  }
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.orangeAccent,
+                                ),
+                                child: const Text('Send Email'),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 100,
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ));
+          ModalProgressHUD(
+            inAsyncCall: state.isLoading,
+            child: Container(),
+          ),
+        ],
+      ),
+    );
   }
 }
