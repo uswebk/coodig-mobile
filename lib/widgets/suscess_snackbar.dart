c32814bc2bb54da41ec487dc06b97d3117971f9c
uswebk <usweb111@gmail.com>
2023-05-01 00:28:41 +0900

    refactor: add snackbar widget

---
+++ lib/widgets/suscess_snackbar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SuccessSnackbar {
+  static void show(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
