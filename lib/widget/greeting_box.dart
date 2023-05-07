120b3b9dfd961bdd04eb0fbebc82eaef3962bb6f
uswebk <usweb111@gmail.com>
2023-05-08 02:05:57 +0900

    refactor: common greeting box

---
+++ lib/widget/greeting_box.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class GreetingBox extends StatelessWidget {
+  final String message;
+  final EdgeInsets padding;
+  final Color backgroundColor;
+  final Color borderColor;
+  final double borderWidth;
+  final double borderRadius;
+  final TextStyle textStyle;
+
+  const GreetingBox({
+    Key? key,
+    required this.message,
+    this.padding = const EdgeInsets.all(10),
+    this.backgroundColor = const Color(0xFFFFEBEE),
+    this.borderColor = const Color(0xFFFFE5E5),
+    this.borderWidth = 1.0,
+    this.borderRadius = 4.0,
+    this.textStyle = const TextStyle(
+      color: Color(0xFFE57373),
+      fontWeight: FontWeight.bold,
+    ),
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: padding,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(borderRadius),
+        border: Border.all(color: borderColor),
+      ),
+      child: Text(
+        message,
+        style: textStyle,
+      ),
+    );
+  }
+}
