28c1eb93c6d07c2aa3a872037d7207f80ef7e961
uswebk <usweb111@gmail.com>
2023-05-20 17:06:55 +0900

    refactor: make instance

--- lib/view/splash/splash_screen.dart
+++ lib/view/splash/splash_screen.dart
@@ -14,8 +14,9 @@ class SplashScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     Future.delayed(const Duration(seconds: 2)).then((value) async {
-      SplashService splashService = SplashService();
+      final SplashService splashService = SplashService();
       splashService.initDeeplink(ref);
+
       await ref.read(authStateProvider.notifier).fetchMe();
       final UserStatus userStatus = ref.watch(userStatusProvider);
       Widget screen = await splashService.getScreen(userStatus);
