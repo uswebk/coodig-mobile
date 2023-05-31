92e171c0ae3cfddd32af426357fe4c3ab6e91b78
uswebk <usweb111@gmail.com>
2023-05-31 23:15:05 +0900

    style: launch design

--- lib/feature/launch/launch_page.dart
+++ lib/feature/launch/launch_page.dart
@@ -1,3 +1,4 @@
+import 'package:coodig_mobile/config/color.dart';
 import 'package:coodig_mobile/enum/user_status.dart';
 import 'package:coodig_mobile/feature/dashboard/dashboard_page.dart';
 import 'package:coodig_mobile/feature/login/login_page.dart';
@@ -16,83 +17,86 @@ class LaunchPage extends ConsumerWidget {
     final UserStatus userStatus = ref.watch(userStatusProvider);
 
     return Scaffold(
-      backgroundColor: Colors.orangeAccent,
+      backgroundColor: CoodigColors.secondary,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: Column(
-                children: [
-                  Column(children: const [
-                    Text(
-                      'Coodig',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 42,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      '- Quiz Application For Engineers -',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ]),
-                  const SizedBox(height: 220),
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            if (userStatus == UserStatus.authenticated) {
-                              Get.off(const DashboardPage());
-                            } else if (userStatus ==
-                                UserStatus.emailNotVerified) {
-                              Get.to(const OtpPage());
-                            } else {
-                              Get.to(const SignupPage());
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.blueAccent,
-                          ),
-                          child: const Text('Sign Up'),
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    Column(children: const [
+                      Text(
+                        'Coodig',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 42,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      const SizedBox(height: 10),
-                      SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: TextButton(
-                          onPressed: () {
-                            if (userStatus == UserStatus.authenticated) {
-                              Get.off(const DashboardPage());
-                            } else {
-                              Get.to(const LoginPage());
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.white70,
+                      SizedBox(height: 10),
+                      Text(
+                        '- Quiz Application For Engineers -',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ]),
+                    const SizedBox(height: 110),
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.9,
+                          height: 60,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (userStatus == UserStatus.authenticated) {
+                                Get.off(const DashboardPage());
+                              } else if (userStatus ==
+                                  UserStatus.emailNotVerified) {
+                                Get.to(const OtpPage());
+                              } else {
+                                Get.to(const SignupPage());
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blueAccent,
+                            ),
+                            child: const Text('Sign Up'),
                           ),
-                          child: const Text(
-                            'Sign In',
-                            style: TextStyle(
-                              color: Colors.black87,
-                              fontWeight: FontWeight.w600,
+                        ),
+                        const SizedBox(height: 15),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.9,
+                          height: 60,
+                          child: TextButton(
+                            onPressed: () {
+                              if (userStatus == UserStatus.authenticated) {
+                                Get.off(const DashboardPage());
+                              } else {
+                                Get.to(const LoginPage());
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white70,
+                            ),
+                            child: const Text(
+                              'Sign In',
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
