066e43af759139223ed0fcfa8edc65d1ffaaf60f
uswebk <usweb111@gmail.com>
2023-07-03 02:01:51 +0900

    refactor: otp verify local loading

--- lib/feature/otp/widgets/verify_button.dart
+++ lib/feature/otp/widgets/verify_button.dart
@@ -2,16 +2,18 @@ import 'package:coodig_mobile/components/snackbar.dart';
 import 'package:coodig_mobile/feature/otp/state/otp_state_notifier.dart';
 import 'package:coodig_mobile/provider/auth_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class VerifyButton extends ConsumerWidget {
+class VerifyButton extends HookConsumerWidget {
   const VerifyButton({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final notifier = ref.read(otpStateNotifierProvider.notifier);
-    final auth = ref.read(authStateProvider.notifier);
     final state = ref.watch(otpStateNotifierProvider);
+    final isProcessing = useState(false);
+    final isMounted = useIsMounted();
+
     return SizedBox(
       width: double.infinity,
       height: 42,
@@ -19,21 +21,29 @@ class VerifyButton extends ConsumerWidget {
         style: ElevatedButton.styleFrom(
           backgroundColor: Theme.of(context).primaryColor,
         ),
-        onPressed: state.isButtonEnabled
+        onPressed: state.isButtonEnabled && isProcessing.value == false
             ? () async {
+                if (isProcessing.value) {
+                  return;
+                }
+                isProcessing.value = true;
                 ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                notifier.setLoading(true);
+                await Future<dynamic>.delayed(const Duration(seconds: 1));
                 try {
-                  await notifier.verify();
-                  await auth.fetchMe();
+                  await ref.read(otpStateNotifierProvider.notifier).verify();
+                  await ref.read(authStateProvider.notifier).fetchMe();
                 } catch (e) {
-                  Snackbar.showError(context, e.toString());
+                  Future.delayed(Duration.zero, () {
+                    Snackbar.showError(context, e.toString());
+                  });
                 } finally {
-                  notifier.setLoading(false);
+                  if (isMounted()) isProcessing.value = false;
                 }
               }
             : null,
-        child: const Text('Verify'),
+        child: isProcessing.value
+            ? const SizedBox(width: 18, height: 18, child: CircularProgressIndicator(strokeWidth: 3))
+            : const Text('Verify'),
       ),
     );
   }
