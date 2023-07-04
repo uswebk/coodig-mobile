79f22b259d93d12dd2855f2b5ea72d459d982f2f
uswebk <usweb111@gmail.com>
2023-07-04 21:27:40 +0900

    refactor: move dashboard if authed

--- lib/feature/login/login_page.dart
+++ lib/feature/login/login_page.dart
@@ -5,22 +5,27 @@ import 'package:coodig_mobile/feature/dashboard/dashboard_page.dart';
 import 'package:coodig_mobile/feature/launch/launch_page.dart';
 import 'package:coodig_mobile/feature/login/state/login_state_notifier.dart';
 import 'package:coodig_mobile/feature/login/widgets/login_form.dart';
+import 'package:coodig_mobile/feature/otp/otp_page.dart';
 import 'package:coodig_mobile/feature/signup/signup_page.dart';
 import 'package:coodig_mobile/provider/auth_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
-class LoginPage extends ConsumerWidget {
+class LoginPage extends HookConsumerWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final UserStatus userStatus = ref.watch(userStatusProvider);
-    if (userStatus == UserStatus.authenticated) {
-      Get.off<dynamic>(const DashboardPage());
-    }
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      final UserStatus userStatus = ref.watch(userStatusProvider);
+      if (userStatus == UserStatus.authenticated) {
+        Get.off<dynamic>(const DashboardPage());
+      } else if (userStatus == UserStatus.emailNotVerified) {
+        Get.off<dynamic>(const OtpPage());
+      }
+    });
 
     final passwordResetEmail = TextEditingController();
 
@@ -44,7 +49,8 @@ class LoginPage extends ConsumerWidget {
             children: [
               Container(
                 alignment: AlignmentDirectional.centerStart,
-                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 60),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 60),
                 child: const Text(
                   'Login',
                   style: TextStyle(
