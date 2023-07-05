a1579fe1ab5b78d55010b6af3a2196c74aecd460
uswebk <usweb111@gmail.com>
2023-07-06 00:10:46 +0900

    refactor: useTextEditingController

--- lib/feature/login/login_page.dart
+++ lib/feature/login/login_page.dart
@@ -10,6 +10,7 @@ import 'package:coodig_mobile/feature/otp/otp_page.dart';
 import 'package:coodig_mobile/feature/signup/signup_page.dart';
 import 'package:coodig_mobile/provider/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -27,7 +28,7 @@ class LoginPage extends HookConsumerWidget {
       }
     });
 
-    final passwordResetEmail = TextEditingController();
+    final passwordResetEmail = useTextEditingController();
 
     return Hud(
       isLoading: ref.watch(loginStateNotifierProvider).isLoading,
