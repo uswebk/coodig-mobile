53d83ac1dfd3fe28b197bf19c4b2fdf757270a78
uswebk <usweb111@gmail.com>
2023-07-07 23:37:08 +0900

    update: make mock

---
+++ test/_/mock.dart
@@ -0,0 +1,6 @@
+import 'package:coodig_mobile/service/deeplink_service.dart';
+import 'package:coodig_mobile/service/splash_service.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateNiceMocks([MockSpec<DeeplinkService>(), MockSpec<SplashService>()])
+void main() {}
