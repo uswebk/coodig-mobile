11aae13b1c83c82e21a2c4628a17443ad041d00d
uswebk <usweb111@gmail.com>
2023-07-11 00:05:07 +0900

    refactor: change color

--- lib/config/color.dart
+++ lib/config/color.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CoodigColors {
-  static const Color primary = Color(0xFF202F55);
-  static const Color secondary = Color(0xFF4DC0B2);
+  static const Color primary = Color(0xFF4DC0B2);
+  static const Color secondary = Color(0xFF202F55);
   static const Color buttonPrimary = Color(0xFF4DC0B2);
   static const Color buttonSecondary = Color(0xFF448AFF);
   static const Color buttonGrey = Color(0xFFA6A6A6);
