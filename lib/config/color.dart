3ab211011b2fec57d0089c28b7ade75c90cad898
uswebk <usweb111@gmail.com>
2023-07-10 22:26:57 +0900

    update: add color

--- lib/config/color.dart
+++ lib/config/color.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 class CoodigColors {
   static const Color primary = Color(0xFF202F55);
   static const Color secondary = Color(0xFF4DC0B2);
-  static const Color buttonPrimary = Color(0xFF448AFF);
+  static const Color buttonPrimary = Color(0xFF4DC0B2);
+  static const Color buttonSecondary = Color(0xFF448AFF);
+  static const Color buttonGrey = Color(0xFFA6A6A6);
   static const Color grey = Color(0xFFA6A6A6);
   static const Color iosLoading = Color(0xFF2B64F6);
   static const Color androidLoading = Color(0xFF448AFF);
