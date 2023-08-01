46a6c1d1df2cc9869f55a738a530324c1e307b7f
uswebk <usweb111@gmail.com>
2023-08-02 00:06:22 +0900

    feature: quiz service

--- lib/service/quiz_service.dart
+++ lib/service/quiz_service.dart
@@ -4,6 +4,7 @@ import 'package:coodig_mobile/model/quiz.dart';
 import 'package:coodig_mobile/repository/quiz_repository.dart';
 import 'package:coodig_mobile/service/auth_service.dart';
 import 'package:coodig_mobile/service/secure_storage_service.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final quizServiceProvider = Provider((ref) => QuizService(
@@ -39,5 +40,38 @@ class QuizService {
     }
 
     return [];
+  }
+
+  Future<QuizAnswer> answer(int quizId, List<int> choiceIds, bool isCorrect) async {
+    String accessToken = await _secureStorageService.getAccessToken();
+    final response = await _quizRepository.answer(quizId, choiceIds, isCorrect, accessToken);
+
+    if (response.statusCode == 201) {
+      final Map<String, dynamic> json = Map<String, dynamic>.from(jsonDecode(response.body) as Map<dynamic, dynamic>);
+
+      return QuizAnswer.fromJson(json);
+    }
+
+    if (response.statusCode == 401) {
+      await _authService.refresh();
+      String accessToken = await _secureStorageService.getAccessToken();
+      final retryResponse = await _quizRepository.answer(quizId, choiceIds, isCorrect, accessToken);
+
+      if (retryResponse.statusCode == 201) {
+        final Map<String, dynamic> json =
+            Map<String, dynamic>.from(jsonDecode(retryResponse.body) as Map<dynamic, dynamic>);
+
+        return QuizAnswer.fromJson(json);
+      }
+    }
+
+    throw Exception('Answer Fail');
+  }
+
+  bool isCorrectQuiz(List<int> answerIds, List<int> choiceIds) {
+    answerIds.sort();
+    choiceIds.sort();
+
+    return listEquals(choiceIds, answerIds);
   }
 }
