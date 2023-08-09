15ded79e6b3e9cc3434da889710d4385e1ff235c
uswebk <usweb111@gmail.com>
2023-08-09 23:15:21 +0900

    refactor: fetch histories if current screen is history

--- lib/feature/history/provider/history_provider.dart
+++ lib/feature/history/provider/history_provider.dart
@@ -1,7 +1,15 @@
+import 'package:coodig_mobile/enum/screen.dart';
 import 'package:coodig_mobile/model/quiz.dart';
+import 'package:coodig_mobile/provider/page_index_provider.dart';
 import 'package:coodig_mobile/service/quiz_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final historyFutureProvider = FutureProvider<List<QuizAnswer>>((ref) async {
-  return await ref.watch(quizServiceProvider).history();
+  ScreenItem screen = ref.watch(screenItemProvider);
+
+  if (screen == ScreenItem.history) {
+    return await ref.watch(quizServiceProvider).history();
+  }
+
+  return [];
 });
