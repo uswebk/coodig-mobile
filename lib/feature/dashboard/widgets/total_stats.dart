4f0620cfe2d8e4cf8f86932a6f2960818aa358b1
uswebk <usweb111@gmail.com>
2023-09-03 19:56:13 +0900

    refactor: total stats design

--- lib/feature/dashboard/widgets/total_stats.dart
+++ lib/feature/dashboard/widgets/total_stats.dart
@@ -14,53 +14,65 @@ class TotalStats extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Column(
       children: [
-        Container(
-          padding: const EdgeInsets.only(left: 10, bottom: 40),
-          child: Row(
-            children: const [
-              Icon(Icons.bar_chart, size: 30, color: CoodigColors.grey),
-              Text('Total Stats', style: TextStyle(color: CoodigColors.grey, fontSize: 24, fontWeight: FontWeight.w600))
-            ],
+        Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: Container(
+            padding: const EdgeInsets.only(left: 25, right: 25, bottom: 30),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Row(
+                    children: const [
+                      Icon(Icons.bar_chart, size: 20, color: CoodigColors.grey),
+                      Text('Stats', style: TextStyle(color: Colors.black54, fontSize: 20, fontWeight: FontWeight.w600)),
+                    ],
+                  ),
+                ),
+                stats.when(
+                    data: (data) {
+                      final double percent = data.correctCount == 0 ? 0 : data.correctCount / data.answerCount;
+                      final String percentLabel = '${(percent * 100).round()}';
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          _stats(data.answerCount.toString(), '', 'answer'),
+                          _stats(data.correctCount.toString(), '', 'correct'),
+                          _stats(percentLabel, '%', 'rate'),
+                        ],
+                      );
+                    },
+                    error: (e, s) => Text(e.toString()),
+                    loading: () => const LoadingIndicator()),
+              ],
+            ),
           ),
         ),
-        stats.when(
-          data: (data) {
-            return Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Column(
-                        children: [
-                          const Text('Answer', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600)),
-                          const SizedBox(height: 10),
-                          Text(
-                            data.answerCount.toString(),
-                            style: const TextStyle(color: Colors.black54, fontSize: 60, fontWeight: FontWeight.w600),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          const Text('Correct', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600)),
-                          const SizedBox(height: 10),
-                          Text(data.correctCount.toString(),
-                              style: const TextStyle(color: Colors.black54, fontSize: 60, fontWeight: FontWeight.w600))
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            );
-          },
-          error: (e, str) => Text(e.toString()),
-          loading: () => const LoadingIndicator(),
-        )
       ],
+    );
+  }
+
+  Widget _stats(String value, String suffix, String subtitle) {
+    return Expanded(
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(value, style: const TextStyle(color: Colors.black87, fontSize: 32, fontWeight: FontWeight.bold)),
+                Text(suffix, style: const TextStyle(color: Colors.grey, fontSize: 16, fontWeight: FontWeight.bold)),
+              ],
+            ),
+            Row(
+              children: [
+                Text(subtitle, style: const TextStyle(color: Colors.black38, fontSize: 14, fontWeight: FontWeight.bold))
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
