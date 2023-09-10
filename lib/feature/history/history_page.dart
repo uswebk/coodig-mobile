182455e15610d0fba5c133c5a561ecdb040701e0
uswebk <usweb111@gmail.com>
2023-09-10 16:25:17 +0900

    update: history page layout

--- lib/feature/history/history_page.dart
+++ lib/feature/history/history_page.dart
@@ -10,71 +10,91 @@ class HistoryPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(historyFutureProvider);
-
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
         backgroundColor: CoodigColors.background,
         body: SafeArea(
           child: Column(
             children: [
               const SizedBox(height: 50),
-              Container(
-                alignment: AlignmentDirectional.centerStart,
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-                child: Row(
-                  children: const [
-                    Icon(Icons.history, size: 30, color: CoodigColors.grey),
-                    SizedBox(width: 10),
-                    Text(
-                      'History',
-                      style: TextStyle(color: CoodigColors.grey, fontSize: 30, fontWeight: FontWeight.w800),
-                    ),
-                  ],
-                ),
-              ),
               Expanded(
                 child: state.when(
                   data: (data) {
                     return ListView.builder(
-                      itemCount: data.length,
-                      itemBuilder: (context, index) => Column(
-                        children: [
-                          Container(
-                            decoration: const BoxDecoration(
-                              color: Colors.white70,
-                              border: Border(bottom: BorderSide(color: Colors.black12, width: 0.5)),
-                            ),
-                            child: ListTile(
-                              leading: Icon(
-                                data[index].isCorrect ? Icons.check_circle : Icons.cancel,
-                                color: data[index].isCorrect ? Colors.green : CoodigColors.error,
-                                size: 18,
-                              ),
-                              title: Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                                child: Text(
-                                  data[index].question.toString(),
-                                  maxLines: 3,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: const TextStyle(fontSize: 12),
+                        itemCount: data.length,
+                        itemBuilder: (context, index) {
+                          final answer = data[index];
+                          return Column(
+                            children: [
+                              Container(
+                                decoration: const BoxDecoration(
+                                  color: Colors.white70,
+                                  border: Border(bottom: BorderSide(color: Colors.black12, width: 0.5)),
+                                ),
+                                child: ListTile(
+                                  leading: Icon(
+                                    answer.isCorrect ? Icons.check_circle : Icons.cancel,
+                                    color: answer.isCorrect ? Colors.green : CoodigColors.error,
+                                    size: 18,
+                                  ),
+                                  title: Padding(
+                                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                                    child: Text(
+                                      answer.question.toString(),
+                                      maxLines: 3,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: const TextStyle(fontSize: 12),
+                                    ),
+                                  ),
+                                  subtitle: Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10),
+                                    child: Align(
+                                        alignment: Alignment.bottomRight,
+                                        child: Text(
+                                          'answer date:${answer.createdAt.year}/${answer.createdAt.month}/${answer.createdAt.day}',
+                                          style: const TextStyle(fontSize: 11),
+                                        )),
+                                  ),
+                                  onTap: () async {
+                                    // show detail bottom sheet
+                                    await showModalBottomSheet<dynamic>(
+                                      shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.vertical(
+                                          top: Radius.circular(30.0),
+                                        ),
+                                      ),
+                                      isScrollControlled: true,
+                                      context: context,
+                                      builder: (context) => Container(
+                                        height: screenHeight * 0.9,
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(40.0),
+                                          child: Column(
+                                            children: [
+                                              const Text('Q'),
+                                              Text(answer.question),
+                                              Expanded(
+                                                child: ListView.builder(
+                                                  itemCount: answer.answerChoices.length,
+                                                  itemBuilder: (BuildContext context, int index) {
+                                                    return ListTile(
+                                                      title: Text(answer.answerChoices[index].choice),
+                                                    );
+                                                  },
+                                                ),
+                                              ),
+                                              Text(answer.question),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  },
                                 ),
                               ),
-                              subtitle: Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10),
-                                child: Align(
-                                    alignment: Alignment.bottomRight,
-                                    child: Text(
-                                      'answer date:${data[index].createdAt.year}/${data[index].createdAt.month}/${data[index].createdAt.day}',
-                                      style: const TextStyle(fontSize: 11),
-                                    )),
-                              ),
-                              onTap: () {
-                                // show detail bottom sheet
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
+                            ],
+                          );
+                        });
                   },
                   error: (e, s) => Text('Error: $e'),
                   loading: () => const Center(child: LoadingIndicator()),
