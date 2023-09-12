c30d54b8d0e505483146fd819b8161f844af6b6d
uswebk <usweb111@gmail.com>
2023-09-12 16:35:19 +0900

    update: history page

--- lib/feature/history/history_page.dart
+++ lib/feature/history/history_page.dart
@@ -1,6 +1,7 @@
 import 'package:coodig_mobile/components/indicator/loading_indicator.dart';
 import 'package:coodig_mobile/config/color.dart';
 import 'package:coodig_mobile/feature/history/provider/history_provider.dart';
+import 'package:coodig_mobile/model/quiz.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -34,7 +35,7 @@ class HistoryPage extends HookConsumerWidget {
                                 child: ListTile(
                                   leading: Icon(
                                     answer.isCorrect ? Icons.check_circle : Icons.cancel,
-                                    color: answer.isCorrect ? Colors.green : CoodigColors.error,
+                                    color: answer.isCorrect ? CoodigColors.success : CoodigColors.error,
                                     size: 18,
                                   ),
                                   title: Padding(
@@ -56,7 +57,9 @@ class HistoryPage extends HookConsumerWidget {
                                         )),
                                   ),
                                   onTap: () async {
-                                    // show detail bottom sheet
+                                    List<QuizAnswerChoice> answerList =
+                                        answer.answerChoices.where((QuizAnswerChoice value) => value.isAnswer).toList();
+
                                     await showModalBottomSheet<dynamic>(
                                       shape: const RoundedRectangleBorder(
                                         borderRadius: BorderRadius.vertical(
@@ -65,25 +68,124 @@ class HistoryPage extends HookConsumerWidget {
                                       ),
                                       isScrollControlled: true,
                                       context: context,
-                                      builder: (context) => Container(
+                                      builder: (context) => SizedBox(
                                         height: screenHeight * 0.9,
                                         child: Padding(
                                           padding: const EdgeInsets.all(40.0),
                                           child: Column(
                                             children: [
-                                              const Text('Q'),
-                                              Text(answer.question),
+                                              Row(
+                                                children: [
+                                                  (answer.isCorrect)
+                                                      ? const Chip(
+                                                          backgroundColor: CoodigColors.success,
+                                                          label: Text('correct', style: TextStyle(color: Colors.white)),
+                                                        )
+                                                      : const Chip(
+                                                          backgroundColor: CoodigColors.error,
+                                                          label:
+                                                              Text('incorrect', style: TextStyle(color: Colors.white)),
+                                                        ),
+                                                ],
+                                              ),
+                                              const SizedBox(height: 10),
+                                              Row(
+                                                children: const [
+                                                  Text('Q.',
+                                                      style: TextStyle(
+                                                          fontSize: 22,
+                                                          color: Colors.blue,
+                                                          fontWeight: FontWeight.bold)),
+                                                ],
+                                              ),
+                                              const Divider(),
                                               Expanded(
+                                                flex: 2,
+                                                child: Padding(
+                                                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 15),
+                                                  child: SingleChildScrollView(child: Text(answer.question)),
+                                                ),
+                                              ),
+                                              const Padding(
+                                                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5),
+                                                child: Divider(),
+                                              ),
+                                              Row(
+                                                children: const [
+                                                  Text(
+                                                    'Choices',
+                                                    style: TextStyle(
+                                                        fontSize: 18, color: Colors.grey, fontWeight: FontWeight.bold),
+                                                  ),
+                                                ],
+                                              ),
+                                              const SizedBox(height: 15),
+                                              Expanded(
+                                                flex: 2,
                                                 child: ListView.builder(
                                                   itemCount: answer.answerChoices.length,
                                                   itemBuilder: (BuildContext context, int index) {
-                                                    return ListTile(
-                                                      title: Text(answer.answerChoices[index].choice),
+                                                    final QuizAnswerChoice answerChoice = answer.answerChoices[index];
+                                                    return Container(
+                                                      decoration: const BoxDecoration(
+                                                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                                                        color: Colors.white,
+                                                      ),
+                                                      margin: const EdgeInsets.symmetric(vertical: 4),
+                                                      child: ListTile(
+                                                        title: Text(
+                                                          answerChoice.choice,
+                                                          style: TextStyle(
+                                                              fontSize: 12,
+                                                              fontWeight:
+                                                                  answerChoice.isSelect ? FontWeight.bold : null),
+                                                        ),
+                                                        subtitle: (answerChoice.isSelect)
+                                                            ? const Text('your selected',
+                                                                style: TextStyle(color: CoodigColors.primary))
+                                                            : null,
+                                                      ),
                                                     );
                                                   },
                                                 ),
                                               ),
-                                              Text(answer.question),
+                                              const Padding(
+                                                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5),
+                                                child: Divider(),
+                                              ),
+                                              Row(
+                                                children: const [
+                                                  Text(
+                                                    'Answers',
+                                                    style: TextStyle(
+                                                        fontSize: 18, color: Colors.grey, fontWeight: FontWeight.bold),
+                                                  ),
+                                                ],
+                                              ),
+                                              const SizedBox(height: 15),
+                                              Expanded(
+                                                flex: 1,
+                                                child: SizedBox(
+                                                  child: ListView.builder(
+                                                    itemCount: answerList.length,
+                                                    itemBuilder: (BuildContext context, int index) {
+                                                      return Container(
+                                                        decoration: const BoxDecoration(
+                                                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                                                          color: Colors.white,
+                                                        ),
+                                                        margin: const EdgeInsets.symmetric(vertical: 4),
+                                                        child: ListTile(
+                                                          title: Text(
+                                                            answerList[index].choice,
+                                                            style: const TextStyle(fontSize: 12),
+                                                          ),
+                                                        ),
+                                                      );
+                                                    },
+                                                  ),
+                                                ),
+                                              ),
                                             ],
                                           ),
                                         ),
